4d0b1b635f73f161241ac50c4309c6d5df402723
p1ck23 <dagobbo17@gmail.com>
2024-08-20 03:58:14 +0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Main/>
+        <Footer/>
+      </BrowserRouter>
     </>
   );
 }
