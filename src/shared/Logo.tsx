85012b5bdf67d522aad8556f9de32d8b60d141d0
p1ck23 <dagobbo17@gmail.com>
2024-08-20 03:56:42 +0400

    create component logo

---
+++ src/shared/Logo.tsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div className="">
+      <img src="/Logo.svg" alt="Logo" className="w-full max-w-xs h-32 mr-2" />
+    </div>
+  );
+};
+
+export default Logo;
