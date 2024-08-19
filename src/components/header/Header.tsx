2e7fcfafaede4283cda385bc2f6c163d71825458
p1ck23 <dagobbo17@gmail.com>
2024-08-19 23:19:49 +0400

    create header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <>
+      <header className="">
+        <a href="">Главная</a>
+        <a href="">Подписки</a>
+        <a href="">Профиль</a>
+      </header>
+    </>
+  );
+};
+
+export default Header;
