ecc714182a16a38f70c81b30c360259b1053eb36
p1ck23 <dagobbo17@gmail.com>
2024-08-24 00:06:02 +0400

    create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import Header from "./components/header/Header";
-import Main from "./components/main/Main";
-import Footer from "./components/footer/Footer";
+import { AppRoutes } from "./AppRoutes";
+import { ModalProvider } from "./context/modalContext";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Main/>
-        <Footer/>
+        <ModalProvider>
+          <AppRoutes />
+        </ModalProvider>
       </BrowserRouter>
     </>
   );
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+
+const Layout: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
